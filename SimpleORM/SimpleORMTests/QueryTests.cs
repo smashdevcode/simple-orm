b1148279e22a4b3bae1613ec0337c9623f54e0de
James Churchill <james@smashdev.com>
2012-06-04 23:24:56 -0700

    Started to setup tests

--- SimpleORM/SimpleORMTests/QueryTests.cs
+++ SimpleORM/SimpleORMTests/QueryTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleORM;
+using SimpleORMTests.Entities;
 
 namespace SimpleORMTests
 {
@@ -7,8 +10,31 @@ namespace SimpleORMTests
 	public class QueryTests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void SelectTest()
 		{
+			// TODO put the expected queries into a set of text files???
+			var expectedQuery = @"
+select CustomerID, Name, Phone
+from Customer;
+";
+			using (var qry = new Query())
+			{
+				qry.Select<Customer>();
+				var result = qry.ExecuteToList();
+				Assert.AreEqual(expectedQuery, qry.SqlQuery);
+				Assert.AreEqual(2, result.Count);
+				// TODO need a helper method to compare collections
+			}
+		}
+		[TestMethod]
+		public void InsertOrUpdateTest()
+		{
+			// TODO
+		}
+		[TestMethod]
+		public void DeleteTest()
+		{
+			// TODO
 		}
 	}
 }
