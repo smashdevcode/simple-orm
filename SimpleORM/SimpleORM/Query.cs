8c91edc252954b7c6ab2f66b0e5bd523cb0d7c08
James Churchill <james@smashdev.com>
2012-06-06 23:14:53 -0700

    First pass at the Query class
    Added additional tests, started setup of test database, first pass at
    the Query class

--- SimpleORM/SimpleORM/Query.cs
+++ SimpleORM/SimpleORM/Query.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace SimpleORM
 {
-    public class Query
+    public class Query<T> : IDisposable where T : class
     {
-    }
+		public string SqlQuery { get; private set; }
+
+		//public Query Select<T>() where T : class
+		//{
+		//	return this;
+		//}
+		public List<T> SelectToList()
+		{
+			BuildQuery();
+
+			// TODO run query against the database
+			// TODO populate a generic collection of materialized entities
+
+			return new List<T>();
+		}
+
+		private void BuildQuery()
+		{
+			var genericType = this.GetType().GetGenericArguments()[0];
+
+			// TODO need to handle collections and reference properties
+
+			// use the properties of the generic type as the select list
+			var properties = genericType.GetProperties();
+			var propertyNames = (from property in properties
+								 where property.CanWrite
+								 select property.Name).ToList();
+			var selectList = string.Join(", ", propertyNames);
+			// use the generic type name as the "from"
+			var fromClause = genericType.Name;
+
+			// build the query
+			var select = new StringBuilder();
+			select.AppendFormat("select {0}{1}", selectList, Environment.NewLine);
+			select.AppendFormat("from {0}", fromClause);
+			this.SqlQuery = select.ToString();
+		}
+
+		public void Dispose()
+		{
+			// TODO dispose of resources
+		}
+	}
 }
--- SimpleORM/SimpleORMTests/QueryTests.cs
+++ SimpleORM/SimpleORMTests/QueryTests.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleORM;
 using SimpleORMTests.Entities;
@@ -9,22 +15,82 @@ namespace SimpleORMTests
 	[TestClass]
 	public class QueryTests
 	{
-		[TestMethod]
-		public void SelectTest()
+		private const string DATABASE_PATH = @"..\\..\\Database\Database.mdf";
+		private const string CONNECTION_STRING = @"Data Source=(LocalDB)\v11.0;AttachDbFilename={0};Integrated Security=True;Connect Timeout=30";
+		private const string QUERIES_DIRECTORY = "..\\..\\Queries";
+
+		private static string GetConnectionString()
 		{
-			// TODO put the expected queries into a set of text files???
-			var expectedQuery = @"
-select CustomerID, Name, Phone
-from Customer;
-";
-			using (var qry = new Query())
+			return string.Format(CONNECTION_STRING, Path.GetFullPath(DATABASE_PATH));
+		}
+		private static string GetQueryText(string methodName)
+		{
+			var fileNameWithPath = Path.GetFullPath(QUERIES_DIRECTORY + "\\" + methodName + ".txt");
+			return File.ReadAllText(fileNameWithPath);
+		}
+		private static bool CompareText(string expected, string actual, string methodName)
+		{
+			// compare strings and write to text file if not equals
+			if (expected != actual)
 			{
-				qry.Select<Customer>();
-				var result = qry.ExecuteToList();
-				Assert.AreEqual(expectedQuery, qry.SqlQuery);
+				var date = DateTime.Now;
+				var expectedFileName = string.Format("{0}_Expected_{1:yyyyMMddhhmmss}.txt", methodName, date);
+				var actualFileName = string.Format("{0}_Actual_{1:yyyyMMddhhmmss}.txt", methodName, date);
+				var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+				File.WriteAllText(desktopPath + "\\" + expectedFileName, expected);
+				File.WriteAllText(desktopPath + "\\" + actualFileName, actual);
+				return false;
+			}
+			return true;
+		}
+
+		#region DatabaseAccessTest
+		[TestMethod]
+		public void DatabaseAccessTest()
+		{
+			var rowValues = new List<string>();
+			using (var conn = new SqlConnection(GetConnectionString()))
+			{
+				conn.Open();
+				using (var cmd = new SqlCommand("select * from Customer", conn))
+				{
+					var reader = cmd.ExecuteReader();
+					var columnNames = (from row in reader.GetSchemaTable().AsEnumerable()
+									   select row.Field<string>("ColumnName")).ToList();
+					while (reader.Read())
+					{
+						var values = (from columnName in columnNames
+									  select string.Format("{0}: {1}", columnName, reader[columnName]));
+						rowValues.Add(string.Join(", ", values));
+					}
+				}
+			}
+			Assert.AreEqual(2, rowValues.Count);
+		}
+		#endregion
+
+		[TestMethod]
+		public void SelectTest_Simple()
+		{
+			using (var qry = new Query<Customer>())
+			{
+				var methodName = MethodBase.GetCurrentMethod().Name;
+				var result = qry.SelectToList();
+				var expectedQuery = GetQueryText(methodName);
+				Assert.IsTrue(CompareText(expectedQuery, qry.SqlQuery, methodName));
 				Assert.AreEqual(2, result.Count);
 				// TODO need a helper method to compare collections
 			}
+		}
+		[TestMethod]
+		public void SelectTest_WithWhere()
+		{
+			// TODO
+		}
+		[TestMethod]
+		public void SelectTest_WithOrderBy()
+		{
+			// TODO
 		}
 		[TestMethod]
 		public void InsertOrUpdateTest()
